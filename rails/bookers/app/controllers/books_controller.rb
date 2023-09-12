a6af3f15d22b7877c21dc3bbc8c65d43c7f7925e
pantomimer <12694276+pantomimer@users.noreply.github.com>
2023-09-12 09:13:10 +0000

    bookersひととおり作成

---
+++ rails/bookers/app/controllers/books_controller.rb
@@ -0,0 +1,48 @@
+class BooksController < ApplicationController
+  def index
+    @book = Book.new
+    @books = Book.all
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to books_path
+    else
+      # flash.now[:alert] = "投稿に失敗しました。"
+      render :index
+    end
+
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+end
