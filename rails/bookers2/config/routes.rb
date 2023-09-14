38e111c6856886ab9cd08ea754422dd2be8f3e19
pantomimer <12694276+pantomimer@users.noreply.github.com>
2023-09-14 18:13:36 +0000

    モデル作成まで

--- rails/bookers2/config/routes.rb
+++ rails/bookers2/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+
+  resources :books
   devise_for :users
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
