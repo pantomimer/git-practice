ba3db4245210c3035be1fe11ad3504b350b345e3
pantomimer <12694276+pantomimer@users.noreply.github.com>
2023-09-15 02:06:34 +0000

    Routesおかしかったので修正

--- rails/bookers2/config/routes.rb
+++ rails/bookers2/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit, :update]
   resources :books
   devise_for :users
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
+  resources :users, only: [:show, :edit, :update]
 
 end
