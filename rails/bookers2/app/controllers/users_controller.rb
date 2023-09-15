0f556a6ba1e5a95c46bab9f970a2148520963b67
pantomimer <12694276+pantomimer@users.noreply.github.com>
2023-09-15 05:47:42 +0000

    共通化前

--- rails/bookers2/app/controllers/users_controller.rb
+++ rails/bookers2/app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 
   def is_matching_login_user
--- rails/bookers2/config/routes.rb
+++ rails/bookers2/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/about', to: 'homes#about', as: 'about'
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update]
 
 end
