755f391669d1c7b4bc01127e932c89012eb897ed
pantomimer <12694276+pantomimer@users.noreply.github.com>
2023-09-16 15:56:07 +0000

    課題6 コメントの非同期通信完了

--- task_rails/2-bookers2-debug/config/routes.rb
+++ task_rails/2-bookers2-debug/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
-    resources :book_comments, only: [:create]
+    resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
     resource :favorites_books, only: [:create, :destroy]
   end
