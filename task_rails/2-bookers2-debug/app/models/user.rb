823c003f0ed01d51af23d1c182757e0f8377c159
pantomimer <12694276+pantomimer@users.noreply.github.com>
2023-09-16 09:13:17 +0000

    課題4 フォロー処理完了

--- task_rails/2-bookers2-debug/app/models/user.rb
+++ task_rails/2-bookers2-debug/app/models/user.rb
@@ -10,10 +10,34 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+  # フォローをした、されたの関係
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # 一覧画面で使う
+  has_many :following_users, through: :followers, source: :followed
+  has_many :follower_users, through: :followeds, source: :follower
+
+
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
 
 
+  #　フォローしたときの処理
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
+
+  #　フォローを外すときの処理
+  def unfollow(user_id)
+    followers.find_by(followed_id: user_id).destroy
+  end
+
+  #フォローしていればtrueを返す
+  def following?(user)
+    following_users.include?(user)
+  end
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
